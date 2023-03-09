a1778e159b194fe8ba5d60a945e1805dc502025a
Tim Pechersky <tim.pechersky@gmail.com>
2023-03-09 11:16:28 +0800

    fix demand options

--- new_wallet.js
+++ new_wallet.js
@@ -4,18 +4,16 @@ var argv = require("yargs/yargs")(process.argv.slice(2))
   .positional("m", {
     describe: "mnemonic for a new wallet",
     type: "string",
-    demandOption: true,
   })
   .positional("p", {
     describe: "password to encrypt key",
     type: "string",
-    demandOption: true,
   })
   .positional("i", {
     describe: "account index to create",
     type: "number",
-    demandOption: true,
-  }).argv;
+  })
+  .demandOption(["p", "m", "i"]).argv;
 
 {
   const MNEMONIC = argv.m;
